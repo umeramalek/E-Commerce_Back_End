7c1b9aaaaad97c2eb5e4e10b4f534b24884a9674
Umera <umeramalek796@gmail.com>
2021-10-28 21:57:50 -0700

    fixed bugs

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -77,7 +77,7 @@ router.delete('/:id', async (req, res) => {
       });
       return;
     }
-    res.status(200).json(categoryResults);
+    res.status(200).json({message:`Category Deleted`});
   }
     catch (err) {
       res.status(500).json(err)
