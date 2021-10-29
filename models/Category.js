59f0bb9de89e7534c10397fd69b3d5ce8548e384
Umera <umeramalek796@gmail.com>
2021-10-28 19:57:54 -0700

    added category model

--- models/Category.js
+++ models/Category.js
@@ -6,7 +6,25 @@ class Category extends Model {}
 
 Category.init(
   {
-  
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    category_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  },
+
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'category',
+  }
 );
 
 module.exports = Category;
