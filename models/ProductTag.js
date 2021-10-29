eccb1e3deb7c0762792e2601c7388c251e9947e7
Umera <umeramalek796@gmail.com>
2021-10-28 20:01:43 -0700

    added product tag model

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -6,7 +6,34 @@ class ProductTag extends Model {}
 
 ProductTag.init(
   {
-    
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id:{
+      type: DataTypes.INTEGER,
+      references:{
+        model:'Product',
+        key:'id',
+      }
+    },
+    tag_id:{
+      type: DataTypes.INTEGER,
+      references:{
+        model:'Tag',
+        key:'id',
+      }
+    },
+  },
+  {
+    sequelize,
+    timestamps: false,
+    freezeTableName: true,
+    underscored: true,
+    modelName: 'product_tag',
+  }
 );
 
 module.exports = ProductTag;
