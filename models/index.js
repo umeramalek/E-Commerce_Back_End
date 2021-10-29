c3d06830e430148fed2afe984790d475f016b815
Umera <umeramalek796@gmail.com>
2021-10-28 20:15:22 -0700

    made relationships in model index.js

--- models/index.js
+++ models/index.js
@@ -7,12 +7,17 @@ const seedProducts = require('../seeds/product-seeds');
 const seedTags = require('../seeds/tag-seeds');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category,{
+  onDelete:'CASCADE',
+  foreignKey: 'category_id'
+})
 // Categories have many Products
-
+Category.hasMany(Product)
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, { through: 'ProductTag',foreignKey:'product_id'});
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: 'ProductTag',foreignKey:'tag_id'})
+
 
 
 
